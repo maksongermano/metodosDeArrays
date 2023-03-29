1ac9a45868d7df47756fd9d331315df66ec74b0e
unknown <maksongermano@gmail.com>
2023-03-29 20:57:29 -0300

    first commit

---
+++ indexOf.js
@@ -0,0 +1,5 @@
+// encontra um item dentro de um array, retorna índice da primeira ocorrência do item procurado.
+
+const lista = ['Eu', 'tu', 'Eles'];
+const indice = lista.indexOf('Tu');
+console.log(indice); //1
